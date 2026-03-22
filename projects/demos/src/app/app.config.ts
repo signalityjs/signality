22756f95a0fec67e4210ae66509771d315c57889
Vyacheslav Borodin <vb-onyx@proton.me>
2026-03-22 23:51:24 +0100

    chore(demos): improve contribution experience

--- projects/demos/src/app/app.config.ts
+++ projects/demos/src/app/app.config.ts
@@ -4,13 +4,23 @@ import {
   provideZonelessChangeDetection,
 } from '@angular/core';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
-import { provideRouter } from '@angular/router';
+import { provideRouter, Routes } from '@angular/router';
+import { Layout } from './layout';
+import { DEMO_ROUTES } from './demo-routes';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: Layout,
+    children: [...DEMO_ROUTES, { path: '', redirectTo: DEMO_ROUTES[0].path, pathMatch: 'full' }],
+  },
+];
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
     provideClientHydration(withEventReplay()),
-    provideRouter([]),
+    provideRouter(routes),
   ],
 };
