a20566f56cdcce4c32a85ca2675c813a34e1f2ff
Vyacheslav Borodin <vb-onyx@proton.me>
2026-08-23 21:30:57 +0200

    chore: update new items

--- projects/docs/.vitepress/config/new-items.ts
+++ projects/docs/.vitepress/config/new-items.ts
@@ -1,6 +1,21 @@
 export const NEW_ITEMS = [
+  '/browser/battery',
+  '/browser/broadcast-channel',
+  '/browser/device-posture',
+  '/browser/favicon',
+  '/browser/fps',
+  '/browser/fullscreen',
+  '/browser/gamepad',
+  '/browser/listener',
+  '/browser/permission-state',
+  '/browser/window-size',
+  '/elements/active-element',
+  '/elements/on-click-outside',
   '/elements/on-key',
-  '/router/query-params',
+  '/elements/scroll-position',
+  '/observers/resize-observer',
+  '/reactivity/throttled',
+  '/scheduling/throttle-callback',
 ];
 
 export function isNew(link: string): boolean {
